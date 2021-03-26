639a6c8dbec8b26cea1b5ead938d50b5e54173ef
Jean Junior <jclgjunior@gmail.com>
2021-03-26 13:47:53 -0300

    feat: Get cars data on Get Clients

--- car_detailing/serializers.py
+++ car_detailing/serializers.py
@@ -4,13 +4,18 @@ from car_detailing.models import Client, Car
 
 
 class ClientSerializer(serializers.ModelSerializer):
+    cars = serializers.SerializerMethodField()
+
     class Meta:
         model = Client
-        fields = ['id', 'name', 'cpf', 'category']
+        fields = ['id', 'name', 'cpf', 'category', 'cars']
+
+    @staticmethod
+    def get_cars(client):
+        return CarSerializer(instance=client.car_set.all(), many=True).data
 
 
 class CarSerializer(serializers.ModelSerializer):
     class Meta:
         model = Car
-        fields = ['id', 'brand_model', 'owner_id', 'category']
-
+        fields = ['id', 'brand_model', 'license_plate', 'color_code', 'category', 'owner']
