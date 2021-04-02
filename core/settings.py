ec874da9c38fe27488368f383e53ba016f89cb17
Jean Junior <jclgjunior@gmail.com>
2021-04-02 13:16:37 -0300

    feat: JWT auth

--- core/settings.py
+++ core/settings.py
@@ -59,6 +59,17 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
+
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework_jwt.views import obtain_jwt_token
 from car_detailing.views import ClientsViewSet, CarViewSet
 
 router = routers.DefaultRouter()
@@ -8,6 +9,7 @@ router.register(r'client', ClientsViewSet)
 router.register(r'car', CarViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include(router.urls))
+    path(r'admin/', admin.site.urls),
+    path('api/login/', obtain_jwt_token),
+    path('api/', include(router.urls)),
 ]
